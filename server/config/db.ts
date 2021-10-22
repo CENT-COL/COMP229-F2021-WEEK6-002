105383cc92c0d4214d968d3669885a67dbd85aea
Thiago Castilho <thiago.castilho@outlook.com>
2021-10-21 20:57:09 -0400

    Allowed Atlas Connection

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,2 +1,3 @@
 export const LocalURI = "mongodb://localhost:27017/portfolio";
+export const RemoteURI = "mongodb+srv://thiago:centennial@cluster0.hdwgd.mongodb.net/portfolio?retryWrites=true&w=majority";
 export const HostName = "localhost";
